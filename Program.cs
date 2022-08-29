b46ba54d736ee9ab9a799a36bd0337ff8be237ac
Spiridonova Victoria <spiridonowa.viky@yandex.ru>
2022-08-29 16:09:36 +0300

    обновление

--- Program.cs
+++ Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите трехзначное число:");
+int number = int.Parse(Console.ReadLine());
+
+while (number < 100 || number > 999)
+    {
+        Console.WriteLine("Данное число не трехзначное,введите трехзначное число:");
+        number = int.Parse(Console.ReadLine()); 
+    }
+    int number1 = number / 10;
+    int number2 = number1 % 10;
+
+    Console.WriteLine("Второй символ введенного числа = " + number2);
