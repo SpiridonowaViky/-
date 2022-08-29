3f2ec89d081144c4969fbab0a60396cfcd7197dc
SpiridonowaViky <110791518+SpiridonowaViky@users.noreply.github.com>
2022-08-29 17:59:41 +0300

    new

--- Program.cs
+++ Program.cs
@@ -1,13 +1,17 @@
-﻿Console.WriteLine("Введите число:");
-string str = Console.ReadLine();
-int n = str.Length;
+﻿Console.WriteLine("Введите цифру, соответствующую дню недели:");
+int number = int.Parse(Console.ReadLine());
 
-if (n > 2)
+while (number<1 || number >7)
 {
-    Console.WriteLine(str [2]);
+    Console.WriteLine("Цифра не соответствует никакому дню недели. Введите цифру, соответствующую дню недели:");
+    number = int.Parse(Console.ReadLine());
+}
+
+if (number>0 && number <6)
+{
+    Console.WriteLine("Рабочий день");
 }
 else
 {
-   Console.WriteLine("Третьей цифры нет");
-};
-
+    Console.WriteLine("Выходной день");
+}
